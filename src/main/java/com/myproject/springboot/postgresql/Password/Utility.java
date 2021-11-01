f1ed5d1303a6103ff40d0576aa0a630ed2fe5fa3
Gurpreet1490 <g.preet08092014@gmail.com>
2021-11-01 14:07:38 +0000

    Initial commit

--- src/main/java/com/myproject/springboot/postgresql/Password/Utility.java
+++ src/main/java/com/myproject/springboot/postgresql/Password/Utility.java
@@ -1,13 +1,10 @@
-package com.myproject.springboot.postgresql.Password;
-
-
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class Utility {
-    public static String getSiteURL(HttpServletRequest request) {
-        String siteURL = request.getRequestURL().toString();
-        return siteURL.replace(request.getServletPath(), "");
-    }
-}
+//package com.myproject.springboot.postgresql.Password;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//public class Utility {
+//    public static String getSiteURL(HttpServletRequest request) {
+//        String siteURL = request.getRequestURL().toString();
+//        return siteURL.replace(request.getServletPath(), "");
+//    }
+//}
