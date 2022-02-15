5d9affe09de8809ef2bc8a910ed1b5afbed43942
Gurpreet1490 <g.preet08092014@gmail.com>
2022-02-15 18:23:50 +0000

    CustomerRepository updated

--- src/main/java/com/myproject/springboot/postgresql/repository/CustomerRepository.java
+++ src/main/java/com/myproject/springboot/postgresql/repository/CustomerRepository.java
@@ -1,9 +1,27 @@
 package com.myproject.springboot.postgresql.repository;
 
 import com.myproject.springboot.postgresql.model.Customer;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.myproject.springboot.postgresql.model.Customer;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
+@Repository
+@Transactional(readOnly = true)
+    public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+        Optional<Customer> findByEmailIdAndPassword(String emailId, String password);
+
+        Optional<Customer> findByFirstNameAndLastName(String firstName, String lastName);
+
+        Optional<Customer> findById(Long customerId);
+       // Optional<Customer> findByEmailId(String emailId);
+
+        Customer findByPasswordToken(String token);
+
+        Customer findByEmailId(String emailId);
+
+    }
